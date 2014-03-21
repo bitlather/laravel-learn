d244c77ded0fb8cb58eb7862718dcaa29b5ed519
Dave Allen <arguma.team+github@gmail.com>
2014-03-20 23:37:36 -0400

    Routes

--- README.md
+++ README.md
@@ -29,5 +29,7 @@ Steps to create authentication
 * Create database for app.
 * Set database credentials in _app/config/database.php_.
 * Create user migration using `php artisan migrate:make create_users_table`
-* Create schema for users table in _app/databae/migrations/yyy_mm_dd_tttttt_create_users_table.php_
+* Create schema for users table in _app/database/migrations/yyy_mm_dd_tttttt_create_users_table.php_
 * Run migration using `php artisan migrate`
+* Create routes in _app/routes.php_
+
--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -11,7 +11,65 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::get('/', function(){ 
+    return View::make('pages/home'); 
 });
+
+Route::get('/account',
+    array('before' => 'auth',  # Only authenticated users can come here
+    function(){ 
+        return View::make('pages/account'); 
+    }
+));
+
+Route::match(
+    array('GET','POST'),       # Allow GET and POST for viewing page and form submission
+    '/log-in',                 # The route
+    array('before' => 'guest', # Authenticated users cannot come here
+    function(){
+        if(Request::isMethod('post')){
+            $credentials = array(
+                'email'     => Input::get('email'),
+                'password'  => Input::get('password')
+                'is_active' => true
+                );
+            if(Auth::attempt($credentials)){
+                # Redirect to previously clicked page if got here by 
+                # Redirect::guest('/log-in'), which happens when 
+                # 'before'=>'auth' filter runs; otherwise go to /account.
+                return Redirect::intended('/account');
+            }
+        }
+        return View::make('pages/log-in');
+    })
+);
+
+Route::get(
+    '/log-out', 
+    function(){
+        Auth::logout();
+        return Redirect::to('/');
+});
+
+Route::match(
+    array('GET', 'POST'),      # Allow GET and POST for viewing page and form submission
+    '/sign-up',                # The route
+    array('before' => 'guest', # Authenticated users cannot come here
+    function(){
+        $validator = null;
+        if(Request::isMethod('post')){
+            $user = new User;
+            $user->first_name = Input::get('first_name');
+            $user->last_name  = Input::get('last_name');
+            $user->email      = Input::get('email');
+            $user->password   = Input::get('password');
+            
+            if($user->save()){
+                Auth::login($user);
+                return Redirect::to('/account');
+            }
+            $validator = $user->validator;
+        }
+        return View::make('pages/sign-up')->withErrors($validator);
+    }
+));
